8a412ad462bafe963bb82bdcd24f3175d38dd166
marcsuets <150144779+marcsuets@users.noreply.github.com>
2024-11-20 10:47:52 +0100

    Update Finished (Animations)

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="TaskList">
-        <Stack.Screen name="TaskList" component={TaskList} options={{headerShown: false ,  animation: 'none'}}/>
-        <Stack.Screen name="CreateTask" component={CreateTask} options={{headerShown: false ,  animation: 'none'}} />
+        <Stack.Screen name="TaskList" component={TaskList} options={{headerShown: false, animation: 'slide_from_right'}}/>
+        <Stack.Screen name="CreateTask" component={CreateTask} options={{headerShown: false, animation: 'slide_from_right'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
